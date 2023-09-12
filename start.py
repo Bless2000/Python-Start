df1215c8a5d2c67bc3863f9a186460738abeef14
Blessed <blessamponsah8@gmail.com>
2023-09-12 10:20:49 +0000

    initialize

---
+++ start.py
@@ -0,0 +1,49 @@
+import os
+os.system('clear')
+
+#############################################
+### Data Types
+# Strings
+# Numbers
+# List
+# Tuples
+# Dictionairies
+# Boolean
+#############################################
+
+
+#Variables
+first_name = "Blessed"
+print(first_name)
+
+first_name = "bob"
+print(first_name)
+
+last_name = "Amponsah"
+
+first_name = last_name
+print(first_name)
+
+#String
+first_name = "bob"
+
+#Number
+Age = 41
+
+#List 
+names = ['John', 'Bob', 'Mary']
+
+print(names)
+
+#Tuples
+objects = ('box', 'chair', 'spoon')
+print(objects)
+
+#Dictionary
+fav_pizza = {
+    'John': 'Pepperoni',
+    'Bob': 'Mushroom',
+    'Mary': 'Cheese'
+}
+print(fav_pizza['John'])
+print(fav_pizza)
