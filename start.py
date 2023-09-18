b602dcab8d87e0d49aa86eca74b988a446fb0499
Blessed <blessamponsah8@gmail.com>
2023-09-18 09:01:27 +0000

    Commit

--- start.py
+++ start.py
@@ -13,37 +13,100 @@ os.system('clear')
 
 
 #Variables
-first_name = "Blessed"
-print(first_name)
+#first_name = "Blessed"
+#print(first_name)
 
-first_name = "bob"
-print(first_name)
+#first_name = "bob"
+#print(first_name)
 
-last_name = "Amponsah"
+#last_name = "Amponsah"
 
-first_name = last_name
-print(first_name)
+#first_name = last_name
+#print(first_name)
 
 #String
-first_name = "bob"
+#first_name = "bob"
 
 #Number
-Age = 41
+#Age = 41
 
 #List 
-names = ['John', 'Bob', 'Mary']
+#names = ['John', 'Bob', 'Mary']
 
-print(names)
+#print(names)
 
 #Tuples
-objects = ('box', 'chair', 'spoon')
-print(objects)
+#objects = ('box', 'chair', 'spoon')
+#print(objects)
 
 #Dictionary
-fav_pizza = {
-    'John': 'Pepperoni',
-    'Bob': 'Mushroom',
-    'Mary': 'Cheese'
-}
-print(fav_pizza['John'])
-print(fav_pizza)
+#fav_pizza = {
+#    'John': 'Pepperoni',
+#    'Bob': 'Mushroom',
+#    'Mary': 'Cheese'
+#}
+#print(fav_pizza['John'])
+#print(fav_pizza)
+
+#greetings = 'My Boss Yelled \"GET BACK TO WORK!\"'
+#print(greetings)
+
+##The Backticks are called escape charactares
+
+#Concatination
+
+#name = 'Bless'
+#greet = "Hello my name is  " + name
+#print(greet)
+
+#age = "14"
+#occ = 'student'
+
+#intro = "Hello my name is " + name + ". I'm " + age + "years old. I'm a " + occ
+
+#print(intro.title())
+#print(intro.swapcase())
+#print(len(intro)) 
+#print(intro[20])
+#print(intro[18:22])
+#print(intro. split(" "))
+
+##########NUmbers###################
+#num = 10.25
+#print(num)
+#print(float(num))
+#print(int(num))
+########################################
+
+########### List ##########################
+#other_list = [1, 2, 3, 4, 5]
+#other_name = 'Jerry'
+#names = ["John", "Bob", "Tina", other_name, other_list]
+
+
+
+#names.append("West")
+
+#print(names)
+#print(len((names)))
+#print(names[len(names)-1])
+
+
+#print(names[0])
+
+#print(names[4])
+#print(names[4][3])
+#print(names[4][3] + 5)
+
+########################## List##################
+
+############ Tuples ############################
+
+name_2 = ("John", "Bob", "Tina")
+
+print(name_2)
+
+
+names.append(40)
+#print(names)
+#print(names[4] + 5)
