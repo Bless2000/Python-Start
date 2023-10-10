5539936cbd6f1fa49c1dff21ac779489790c1a70
Blessed <blessamponsah8@gmail.com>
2023-10-10 11:32:09 +0000

    commit

---
+++ .vscode/kinter.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+#Creating a label widget
+mylabel = Label(root, text="Hello World!")
+#Shoving it onto the screen
+mylabel.pack()
+
+
+root.mainloop()
--- exe.py
+++ exe.py
@@ -7,8 +7,63 @@ List = ['apple', 'banana', 'watermelon']
 print(List)
 
 List.append("orange")
+
+List1 = ["shirt", "shorts", "trousers", "skirt"]
+List1.insert(3, "jacket")
+print(List1)
+
+List.extend(List1)
+
+List1.remove("shorts")
+print(List1)
+
+#Remove a specific index
+List.pop(2)
+
 print(List)
-print(len(List))
+
+#Romoving the last item
+List.pop()
+print(List)
+
+#delete the list completely
+List2 = ["shirt", "shorts", "trousers", "skirt"]
+#del List2
+
+List2.clear()
+print(List2)
+
+#Loop through a List
+for x in List:
+    print(x)
+
+#Looping through the indexes or index numbers
+for i in range(len(List)):
+    print(List[i])
+
+#While Loop
+i = 0
+while i > len(List1):
+    print(List1[i])
+    i = i + 1
+
+#Looping using List Comprehension (Short-Hand for writing loops)
+
+[print(t) for t in List1]
+
+#Sorting
+List.sort()
+print(List)
+
+#Sorting list numerically
+thislist = [1, 50, 62, 34, 71, 100]
+thislist.sort()
+print(thislist)
+
+#Sorting descending other
+List1.sort(reverse = True)
+print(List1)
+
 
 dictionary = {
     "name": "Blessed",
@@ -16,3 +71,4 @@ dictionary = {
     "height": "6'2"
 }
 print(dictionary)
+
